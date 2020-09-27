e1066aa9d3cbca2f3f2cef0658acd5bf5f06554c
Nicole Gile <nicolegile94@gmail.com>
2020-09-26 23:15:13 -0500

    fix Manager test

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -2,13 +2,13 @@ const Employee = require('../lib/Employee.js');
 const Manager = require('../lib/Manager');
 
 test('creates a Manager object', () => {
-    const manager = new Manager();
+    const manager = new Manager('testName', 'testID', 'testEmail', 'testOfficeNumber', 'testRole');
 
-    expect(manager.officeNumber).toEqual(expect.any(Number));
+    expect(manager.officeNumber).toBe('testOfficeNumber');
 });
 
 test("gets Manager's role", () => {
-    const manager = new Manager();
+    const manager = new Manager('testName', 'testID', 'testEmail', 'testOfficeNumber', 'testRole');
 
-    expect(manager.getRole()).toHaveProperty('role');
+    expect(manager.getRole()).toBe('Manager');
 })
